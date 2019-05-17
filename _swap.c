08f36422b0f9adeac217bc4b863655a1f2ceb971
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-17 04:53:48 +0000

    [Fix] Betty on _swap

--- _swap.c
+++ _swap.c
@@ -1,19 +1,17 @@
 #include "monty.h"
-
 /**
-* _swap: entry point of the function
+* _swap - entry point of the function
 * @stack: stack that receives
 * @line_number: Number that receives
 * Return: 0 if sucess
 */
-
 void _swap(stack_t **stack, unsigned int line_number)
 {
 	int i;
 
-	for(i = 0; i < 20; i++)
+	for (i = 0; i < 20; i++)
 	{
-		if(len(stack) < 2)
+		if (len(stack) < 2)
 		{
 		printf("L%d: can't swap, stack too short", i);
 		}
