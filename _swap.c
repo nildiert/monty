228fa6cac3a9d9a9fc61abf7bf7710aeb04fee41
nildiert <niljordan23@gmail.com>
2019-05-17 04:54:03 +0000

    [FEATURE] Fixed

--- _swap.c
+++ _swap.c
@@ -1,21 +1,46 @@
 #include "monty.h"
-
 /**
-* _swap: entry point of the function
-* @stack: stack that receives
-* @line_number: Number that receives
-* Return: 0 if sucess
-*/
-
-void _swap(stack_t **stack, unsigned int line_number)
+ *Swap - function that swaps the values in the list
+ *@stack: pointer to the stack
+ *@line_number: the number of lines that recives
+ *Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
 {
-	int i;
+	stack_t *tmp1;
+	stack_t *tmp2;
+	int value;
+	int len;
 
-	for(i = 0; i < 20; i++)
+	len = len_stack(stack);
+
+	if (len < 2)
 	{
-		if(len(stack) < 2)
-		{
-		printf("L%d: can't swap, stack too short", i);
-		}
+		fprintf(stderr, "can't swap, stack too short %u\n", line_number);
 	}
+	tmp1 = (*stack);
+	tmp2 = tmp1->next;
+	value = tmp1->n;
+	tmp1->n = tmp2->n;
+	tmp2->n = value;
+}
+/**
+ *len_stack - return the len of the arg list
+ *@stack: pointer to the stack
+ *Return: size
+ */
+int len_stack(stack_t **stack)
+{
+        stack_t *counter;
+        int size = 0; 
+
+        counter = *stack;
+        while (counter != NULL)
+        {
+                counter = counter->next;
+                size++;
+        }
+
+        return (size);
+
 }
