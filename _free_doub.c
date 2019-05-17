5e464acef63374edb35bae637a4a6220cfba288c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-17 20:01:14 +0000

    [Fix] Fixed Betty style issues

--- _free_doub.c
+++ _free_doub.c
@@ -1,8 +1,7 @@
 #include "monty.h"
 /**
- * _free_doub - tokenize and return an array of tokens
- *@line: the file to cut
- *
+ *_free_doub - tokenize and return an array of tokens
+ *@args: args received
  * Return: array of tokens.
  */
 void _free_doub(char **args)
@@ -12,10 +11,4 @@ void _free_doub(char **args)
 	(void)j;
 	(void)i;
 	free(args);
-/*	for(i = 0; *(args + i); i++)
-	{
-		for (j = 0; *(args + j); j++)
-			free(args[j]);*/
-/*			printf("::DEBUGG free_doub ::%s\n", args[j]);
-	}*/
 }
